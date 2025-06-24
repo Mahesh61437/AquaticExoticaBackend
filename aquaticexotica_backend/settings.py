8d43c6a48ec475046d46512d3d63231292a32380
Mahesh Babu <mahesh@Maheshs-MacBook-Pro.local>
2025-06-25 00:26:53 +0530

    disabling pagination

--- aquaticexotica_backend/settings.py
+++ aquaticexotica_backend/settings.py
@@ -169,8 +169,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': None,
+    # 'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
