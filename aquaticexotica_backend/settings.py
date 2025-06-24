69de9d5c39310f437e07b1792c36be0cbd39e4b6
Mahesh Babu <mahesh@Maheshs-MacBook-Pro.local>
2025-06-24 17:10:12 +0530

    Changing redis endpoint

--- aquaticexotica_backend/settings.py
+++ aquaticexotica_backend/settings.py
@@ -174,7 +174,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",  # DB 1 for Django
+        "LOCATION": os.getenv("REDIS_URL", default="redis://127.0.0.1:6379/1"),  # DB 1 for Django
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
