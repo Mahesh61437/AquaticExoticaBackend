d33c2a40824e08f6103c9f37bc6b936514741fc4
Mahesh Babu <mahesh@Maheshs-MacBook-Pro.local>
2025-06-25 15:43:25 +0530

    Adding optinal slash router v2

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,39 @@
 from django.urls import path, include, re_path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
+
 from .views import (
     ProductViewSet, CategoryViewSet, OrderViewSet,
     ContactView, StockNotificationSubscribeView,
     StockNotificationNotifyView
 )
 
-class OptionalSlashRouter(DefaultRouter):
-    trailing_slash = '/?'  # Optional trailing slash
+
+class OptionalSlashRouter(SimpleRouter):
+    def __init__(self, *args, **kwargs):
+        self.trailing_slash = '/?'  # Use this for generating regex
+        super().__init__(*args, **kwargs)
+
+    def get_urls(self):
+        # Override to use re_path with optional trailing slash
+        ret = []
+        routes = self.get_routes(None)  # We pass None because we patch it later in the loop
+
+        for prefix, viewset, basename in self.registry:
+            lookup = self.get_lookup_regex(viewset)
+            for route in self.get_routes(viewset):
+                mapping = self.get_method_map(viewset, route.mapping)
+                if not mapping:
+                    continue
+                regex = route.url.format(
+                    prefix=prefix,
+                    lookup=lookup,
+                    trailing_slash=self.trailing_slash
+                )
+                view = viewset.as_view(mapping, **route.initkwargs)
+                name = route.name.format(basename=basename)
+                ret.append(re_path(regex, view, name=name))
+        return ret
+
 
 router = OptionalSlashRouter()
 router.register(r'products', ProductViewSet, basename='product')
